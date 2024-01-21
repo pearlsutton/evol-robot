8c1e757ecfcc6085526c76bc162e5e9718253dc0
pearlsutton <psutton1@uvm.edu>
2024-01-21 14:45:31 -0500

    replace positions with x,y,z variables

--- generate.py
+++ generate.py
@@ -6,6 +6,10 @@ length = 1
 width = 2
 height = 3
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
+x = 1
+y = 1
+z= 1
+
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
 pyrosim.End()

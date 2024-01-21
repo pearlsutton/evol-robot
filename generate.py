cb4cf8c433920cbe43e824a5488b5b2ad5d79480
pearlsutton <psutton1@uvm.edu>
2024-01-21 14:52:38 -0500

    1 meter box and ground level

--- generate.py
+++ generate.py
@@ -3,12 +3,12 @@ import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
-x = 1
-y = 1
-z = 1.5
+x = 0
+y = 0
+z = 0.5
 
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 

bb9199b86fabbc555cd2c53f333817d8f92dc22f
pearlsutton <psutton1@uvm.edu>
2024-01-21 13:43:00 -0500

    adding force of gravity to world

--- simulate.py
+++ simulate.py
@@ -3,6 +3,9 @@ import time
 
 physicsClient = p.connect(p.GUI)
 
+# Determining forces that exist in our world. Gravity.
+p.setGravity(0,0,-9.8)
+# Information about our world.
 p.loadSDF("box.sdf")
 
 for x in range (1000):

1472c53ddd075efffb8307aee610c5d5989880a1
pearlsutton <psutton1@uvm.edu>
2024-01-29 11:58:39 -0500

    second box starts next to and right above first box.

--- generate.py
+++ generate.py
@@ -11,6 +11,6 @@ y = 0
 z = 0.5
 
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[x,y+1,z+1] , size=[length,width,height])
 
 pyrosim.End()

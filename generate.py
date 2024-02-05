b590831a5f32c77b293f7907b9641447672ecd80
pearlsutton <psutton1@uvm.edu>
2024-02-05 12:37:45 -0500

    correct 3 links and joints robot.

--- generate.py
+++ generate.py
@@ -19,8 +19,8 @@ def Create_Robot():
     pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,0,1])
     pyrosim.Send_Cube(name="BackLeg", pos=[-1,0,-.5] , size=[length,width,height])
 
-    pyrosim.Send_Joint( name = "BackLeg_FrontLeg" , parent= "BackLeg" , child = "FrontLeg" , type = "revolute", position = [1,0,0])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0,0,-.5] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "BackLeg_FrontLeg" , parent= "BackLeg" , child = "FrontLeg" , type = "revolute", position = [-.5,0,-.5])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[1.5,0,0] , size=[length,width,height])
 
     pyrosim.End()
 

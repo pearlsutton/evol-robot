2304877a84575934b159d46fb80cb9af75548777
pearlsutton <psutton1@uvm.edu>
2024-02-07 14:11:18 -0500

    sine graph.

--- analyze.py
+++ analyze.py
@@ -3,12 +3,18 @@ import matplotlib.pyplot
 
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy', mmap_mode=None, allow_pickle=False)
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy', mmap_mode=None, allow_pickle=False)
+targetAngles = numpy.load('data/targetAngles.npy', mmap_mode=None, allow_pickle=False)
 
-print(backLegSensorValues)
-print(frontLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues, label='BackLeg Sensor Values', linewidth = 2.5)
-matplotlib.pyplot.plot(frontLegSensorValues, label='FrontLeg Sensor Values')
+# print(backLegSensorValues)
+# print(frontLegSensorValues)
+print(targetAngles)
+
+# matplotlib.pyplot.plot(backLegSensorValues, label='BackLeg Sensor Values', linewidth = 2.5)
+# matplotlib.pyplot.plot(frontLegSensorValues, label='FrontLeg Sensor Values')
+matplotlib.pyplot.plot(targetAngles, label='Target Angles')
 
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
+
+
--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
 
 physicsClient = p.connect(p.GUI)
 
@@ -17,11 +18,12 @@ robotId = p.loadURDF("body.urdf")  # simulate.py tells pybullet to simulate a ro
 p.loadSDF("world.sdf")             # simulate.py tells pybullet to simulate a world stored in world.sdf. Information about our world.
 
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(10000)
-frontLegSensorValues = numpy.zeros(10000)
+backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
 
+targetAngles = (numpy.sin(numpy.linspace(0, 2*numpy.pi, 10000)))
 
-for x in range (10000):
+for x in range (100):
     time.sleep(1/6000)
     p.stepSimulation()
     backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
@@ -30,17 +32,18 @@ for x in range (10000):
         bodyIndex = robotId,
         jointName = b'Torso_BackLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = -numpy.pi/4.0,
+        targetPosition = random.uniform(-numpy.pi/20.0, numpy.pi/20.0),
         maxForce = 500)
     pyrosim.Set_Motor_For_Joint(
         bodyIndex = robotId,
         jointName = b'Torso_FrontLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = numpy.pi/4.0,
+        targetPosition = random.uniform(-numpy.pi/20.0, numpy.pi/20.0),
         maxForce = 500)
     print(backLegSensorValues)
     print(frontLegSensorValues)
 
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues, allow_pickle=True, fix_imports=True)
 numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues, allow_pickle=True, fix_imports=True)
+numpy.save('data/targetAngles.npy', targetAngles, allow_pickle=True, fix_imports=True)
 p.disconnect()

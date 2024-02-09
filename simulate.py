755c00e98420d83339fd31d4f007bca8b654bb22
pearlsutton <psutton1@uvm.edu>
2024-02-08 21:25:31 -0500

    new variables: amplitude, frequency, phaseOffset.

--- simulate.py
+++ simulate.py
@@ -3,25 +3,23 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
-import random
+
+amplitude = numpy.pi/4.0
+frequency = 1
+phaseOffset = 0
 
 # physicsClient = p.connect(p.DIRECT)
 physicsClient = p.connect(p.GUI)
-
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
 # Determining forces that exist in our world.
-# Gravity.
-p.setGravity(0,0,-9.8)
-# Floor.
-planeId = p.loadURDF("plane.urdf")
+p.setGravity(0,0,-9.8)  # Gravity.
+planeId = p.loadURDF("plane.urdf")  # Floor.
 robotId = p.loadURDF("body.urdf")  # simulate.py tells pybullet to simulate a robot stored in body.urdf.
 p.loadSDF("world.sdf")             # simulate.py tells pybullet to simulate a world stored in world.sdf. Information about our world.
 
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
-# targetAngles = numpy.zeros(100)
 targetAngles = numpy.sin(numpy.linspace(0, 2*numpy.pi, 1000)) * numpy.pi/4.0
 
 for x in range (1000):

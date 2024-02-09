0208cc0589cfcc2393f3d4b7e22fc2a79b768ad8
pearlsutton <psutton1@uvm.edu>
2024-02-09 15:24:10 -0500

    speeding up robot as much as possible.

--- simulate.py
+++ simulate.py
@@ -5,9 +5,13 @@ import pyrosim.pyrosim as pyrosim
 import numpy
 import array
 
-amplitude = numpy.pi/4.0
-frequency = 10
-phaseOffset = 0
+amplitudeBackLeg = numpy.pi/4.0
+frequencyBackLeg = 10
+phaseOffsetBackLeg = 0
+
+amplitudeFrontLeg = numpy.pi/4.0
+frequencyFrontLeg = 10
+phaseOffsetFrontLeg = numpy.pi/6.0
 
 # physicsClient = p.connect(p.DIRECT)
 physicsClient = p.connect(p.GUI)
@@ -22,7 +26,8 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
 
-targetAngles = amplitude * numpy.sin(numpy.linspace(frequency*0 + phaseOffset, frequency*2*numpy.pi + phaseOffset, 1000))
+targetAnglesBackLeg = amplitudeBackLeg * numpy.sin(numpy.linspace(frequencyBackLeg*0 + phaseOffsetBackLeg, frequencyBackLeg*2*numpy.pi + phaseOffsetBackLeg, 1000))
+targetAnglesFrontLeg = amplitudeFrontLeg * numpy.sin(numpy.linspace(frequencyFrontLeg*0 + phaseOffsetFrontLeg, frequencyFrontLeg*2*numpy.pi + phaseOffsetFrontLeg, 1000))
 
 for x in range (1000):
     time.sleep(1/240)
@@ -31,20 +36,23 @@ for x in range (1000):
         bodyIndex = robotId,
         jointName = b'Torso_BackLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = targetAngles[x],
+        targetPosition = targetAnglesBackLeg[x],
         maxForce = 500)
     pyrosim.Set_Motor_For_Joint(
         bodyIndex = robotId,
         jointName = b'Torso_FrontLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = targetAngles[x],
+        targetPosition = targetAnglesFrontLeg[x],
         maxForce = 500)
     backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    print(backLegSensorValues)
-    print(frontLegSensorValues)
+    # print(backLegSensorValues)
+    # print(frontLegSensorValues)
 
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues, allow_pickle=True, fix_imports=True)
 numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues, allow_pickle=True, fix_imports=True)
-numpy.save('data/targetAngles.npy', targetAngles, allow_pickle=True, fix_imports=True)
+
+numpy.save('data/targetAnglesBackLeg.npy', targetAnglesBackLeg, allow_pickle=True, fix_imports=True)
+numpy.save('data/targetAnglesFrontLeg.npy', targetAnglesFrontLeg, allow_pickle=True, fix_imports=True)
+
 p.disconnect()

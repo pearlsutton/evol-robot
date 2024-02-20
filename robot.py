a0faa76e0453cff62e431075b0356940f9baf1fd
pearlsutton <psutton1@uvm.edu>
2024-02-19 19:48:38 -0500

    step 45: Prepare_To_Sense() in ROBOT class

--- robot.py
+++ robot.py
@@ -9,3 +9,5 @@ class ROBOT:
 
         self.robotId = p.loadURDF("body.urdf")  # simulate.py tells pybullet to simulate a robot stored in body.urdf.
         pyrosim.Prepare_To_Simulate(self.robotId)
+
+    def Prepare_To_Sense():

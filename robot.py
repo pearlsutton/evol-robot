d4f320748a0791bfa2fefdb446ec14a4da99c8c5
pearlsutton <psutton1@uvm.edu>
2024-02-27 11:59:26 -0500

    step 76: replace t with desiredAngle.

--- robot.py
+++ robot.py
@@ -30,11 +30,11 @@ class ROBOT:
     def Act(self, t):
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
-                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 print(neuronName, jointName, desiredAngle)
-        for i in self.motors:
-            self.motors[i].Set_Value(self.robotId, t)
+        # for i in self.motors:
+        #     self.motors[i].Set_Value(self.robotId, desiredAngle)
 
     def Think(self):
         self.nn.Update()

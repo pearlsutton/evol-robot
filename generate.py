6d35c190fc1adfdef31d73c5791ca6203fc3dcc3
pearlsutton <psutton1@uvm.edu>
2024-02-27 08:29:46 -0500

    step 14: brain.nndf update

--- generate.py
+++ generate.py
@@ -26,6 +26,7 @@ def Generate_Body():
 
 def Generate_Brain():
     pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
     pyrosim.End()
 
 Create_World()

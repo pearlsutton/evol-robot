16c639c74f9b8d2ff5d93c8e41718b9227e25cab
pearlsutton <psutton1@uvm.edu>
2024-02-26 19:43:01 -0500

    step 80: set amp, freq, and offset to constants

--- motor.py
+++ motor.py
@@ -1,5 +1,25 @@
 import string
+import constants as c
 
 class MOTOR:
     def __init__(self, jointName: string):
         self.jointName = jointName
+        self.Prepare_To_Act()
+
+    def Prepare_To_Act(self):
+        pass
+        self.amplitude = c.amplitudeBackLeg
+        self.frequency = c.frequencyBackLeg
+        self.offset = c.phaseOffsetBackLeg
+    #     pyrosim.Set_Motor_For_Joint(
+    #         bodyIndex = self.robot,
+    #         jointName = b'Torso_BackLeg',
+    #         controlMode = p.POSITION_CONTROL,
+    #         targetPosition = c.targetAnglesBackLeg[t],
+    #         maxForce = 50)
+    #     pyrosim.Set_Motor_For_Joint(
+    #         bodyIndex = self.robot,
+    #         jointName = b'Torso_FrontLeg',
+    #         controlMode = p.POSITION_CONTROL,
+    #         targetPosition = c.targetAnglesFrontLeg[t],
+    #         maxForce = 50)

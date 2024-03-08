e30b8536f30d7b1afbb90105ae8e7d704ee2e106
pearlsutton <psutton1@uvm.edu>
2024-03-08 15:12:00 -0500

    running search produces 6 random solutions w/ currentRow & currentColumn.

--- solution.py
+++ solution.py
@@ -1,20 +1,23 @@
 import numpy
 import pyrosim.pyrosim as pyrosim
+import os
 
 class SOLUTION:
     def __init__(self):
         self.weights = 2 * numpy.random.rand(3, 2) - 1
 
     def Evaluate(self):
-
+        SOLUTION.Create_World(self)
+        SOLUTION.Create_Robot(self)
+        SOLUTION.Create_Brain(self)
     def Create_World(self):
         pass
         # pyrosim.Start_SDF("world.sdf")
         # pyrosim.Send_Cube(name="Box", pos=[2,2,z] , size=[length,width,height])
         # pyrosim.End()
-     def Create_Robot(self):
+    def Create_Robot(self):
          pass
-    def Generate_Brain(self):
+    def Create_Brain(self):
         pyrosim.Start_NeuralNetwork("brain.nndf")
 
         pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
@@ -25,12 +28,12 @@ class SOLUTION:
         pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 
         sensor_neurons_list = [0 , 1 , 2]
-        currentRow = numpy.array(sensor_neurons_list)
-        motor_neurons_list = [3, 4]
-        currentColumn = numpy.array(motor_neurons_list)
+        sensor_neurons_vector = numpy.array(sensor_neurons_list)
+        motor_neurons_list = [0, 1]
+        motor_neurons_vector = numpy.array(motor_neurons_list)
 
-        for i in currentRow:
-            for j in currentColumn:
-                pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = random.uniform(-1,1) )
+        for currentRow in sensor_neurons_vector:
+            for currentColumn in motor_neurons_vector:
+                pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn+3 , weight = self.weights[currentRow][currentColumn] )
 
         pyrosim.End()

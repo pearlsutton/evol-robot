a92443ba909701a44b36d953895672ce8e13c6d2
pearlsutton <psutton1@uvm.edu>
2024-03-08 14:34:55 -0500

    self.weights * 2 - 1.

--- solution.py
+++ solution.py
@@ -2,6 +2,6 @@ import numpy
 
 class SOLUTION:
     def __init__(self):
-        self.weights = numpy.random.rand(3, 2)
+        self.weights = 2 * numpy.random.rand(3, 2) - 1
         print(self.weights)
         exit()

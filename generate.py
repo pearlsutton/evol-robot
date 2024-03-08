8d3a1e301612cbd483d700a310ec3f690bf87204
pearlsutton <psutton1@uvm.edu>
2024-03-08 12:14:50 -0500

    step 7 & 8: loops over sensor and motor neurons.

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 length = 1
 width = 1
@@ -26,6 +27,7 @@ def Generate_Body():
 
 def Generate_Brain():
     pyrosim.Start_NeuralNetwork("brain.nndf")
+
     pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
     pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
@@ -33,11 +35,14 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 
-    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -4.0 )
-    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -4.0 )
+    sensor_neurons_list = [0 , 1 , 2]
+    sensor_neurons_vector = numpy.array(sensor_neurons_list)
+    motor_neurons_list = [3, 4]
+    motor_neurons_vector = numpy.array(motor_neurons_list)
 
-    pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1.0 )
-    pyrosim.Send_Synapse( sourceNeuronName = 3 , targetNeuronName = 4 , weight = 1.0 )
+    for i in sensor_neurons_vector:
+        for j in motor_neurons_vector:
+            pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = 1 )
 
     pyrosim.End()
 
